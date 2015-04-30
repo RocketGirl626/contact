2cef7b649a08365881d1135861d3b29d7cb842fc
Guest User <student@epicodus.com>
2015-04-30 09:53:40 -0700

    pass the name spec with the initialize, save, and clear methods

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,7 +1,16 @@
 class Contact
   @@contacts = []
-  attr_reader()
+  attr_reader(:name)
+  
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
+  end
+
+  define_singleton_method(:clear) do
+    @@contacts = []
+  end
+
+  define_method(:save) do
+    @@contacts.push(self)
   end
 end
