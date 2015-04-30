3bd843dca7b9208398d8be9f1944f6211650a6f7
Guest User <student@epicodus.com>
2015-04-30 15:03:38 -0700

    pass spec for zip code in the Address

--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -13,6 +13,27 @@ describe(Address) do
     end
   end
 
+  describe('#city') do
+    it('returns the city of the Address') do
+      test_address = Address.new({:address_line => '11640 NW Stone Mountain Lane', :city => 'Portland', :state => 'OR', :zip => '97229' })
+      expect(test_address.city()).to(eq('Portland'))
+    end
+  end
+
+  describe('#state') do
+    it('returns the state of the Address') do
+      test_address = Address.new({:address_line => '11640 NW Stone Mountain Lane', :city => 'Portland', :state => 'OR', :zip => '97229'})
+      expect(test_address.state()).to(eq('OR'))
+    end
+  end
+
+  describe('#zip') do
+    it('returns the zip code of the Address') do
+      test_address = Address.new({:address_line => '15130 SW Millikan Way', :city => 'Beaverton', :state => 'OR', :zip => '97003'})
+      expect(test_address.zip()).to(eq('97003'))
+    end
+  end
+
   describe('.all') do
     it('is empty at first') do
       expect(Address.all()).to(eq([]))
